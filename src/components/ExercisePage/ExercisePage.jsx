d677aef2e90cd4704a91c1f2bab082d73ac782d9
Lucas Houska <ljhouska75@gmail.com>
2022-04-17 19:32:09 -0500

    added to rendering on exercise page and get query

--- src/components/ExercisePage/ExercisePage.jsx
+++ src/components/ExercisePage/ExercisePage.jsx
@@ -2,6 +2,14 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 
 function ExercisePage() {
 
@@ -10,7 +18,7 @@ function ExercisePage() {
     const workoutId = useParams().workoutId;
     const exerciseNumber = useParams().exerciseNumber;
     const exerciseList = useSelector(state => state.workout.exerciseList)
-    
+
 
     useEffect(() => {
         dispatch({ type: `GET_WORKOUT`, payload: { workoutId, exerciseNumber } }) //
@@ -18,16 +26,28 @@ function ExercisePage() {
 
     return (
         <>
-            <h1>Hello!</h1>
-            <ul>
-                {exerciseList.map(exercise => {
-                    return (
-                        <>
-                            <li>{exercise.exercise_id}</li>
-                        </>
-                    )
-                })}
-            </ul>
+            <h1>{exerciseList[0]?.exercise_name.toUpperCase()}</h1>
+
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">Sets</TableCell>
+                            <TableCell align="center">Reps&nbsp;</TableCell>
+                            <TableCell align="center">Weight&nbsp;</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {exerciseList.map((exercise) => (
+                            <TableRow key={exercise?.id}>
+                                <TableCell align="center">{exercise?.set_number}</TableCell>
+                                <TableCell align="center">{exercise?.repetitions}</TableCell>
+                                <TableCell align="center">{exercise?.weight}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     )
 }
