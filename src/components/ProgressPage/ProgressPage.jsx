a30645c54a09119f85e02b64c488b289074ed01e
Lucas Houska <ljhouska75@gmail.com>
2022-04-25 20:57:30 -0500

    added Recharts

--- src/components/ProgressPage/ProgressPage.jsx
+++ src/components/ProgressPage/ProgressPage.jsx
@@ -1,28 +1,20 @@
-import { Line } from 'react-chartjs-2';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 function ProgressPage() {
+
+    const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }];
+
     return (
         <>
             <div id="progress-page">
                 <h1>Your Progression</h1>
-                <Line
-                    datasetIdKey='id'
-                    data={{
-                        labels: ['Jun', 'Jul', 'Aug'],
-                        datasets: [
-                            {
-                                id: 1,
-                                label: '',
-                                data: [5, 6, 7],
-                            },
-                            {
-                                id: 2,
-                                label: '',
-                                data: [3, 2, 1],
-                            },
-                        ],
-                    }}
-                />
+                <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                </LineChart>
             </div>
         </>
     )
