20406cd0ec980ae545cdabee622725c227be2f07
Lucas Houska <ljhouska75@gmail.com>
2022-04-26 08:58:51 -0500

    readded progress router and pool that was accidentally deleted

--- server/routes/workout.router.js
+++ server/routes/workout.router.js
@@ -4,6 +4,19 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 
+router.get('/program', rejectUnauthenticated, (req, res) => {
+    const queryText = `
+    SELECT * FROM "program" 
+    JOIN "exercises" ON "program".exercise_id = "exercises".id`
+
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log(error);
+        res.sendStatus(500);
+    })
+})
+
 router.get('/:workoutId', rejectUnauthenticated, (req, res) => {
 
     console.log('workout id in get', req.params.workoutId);
