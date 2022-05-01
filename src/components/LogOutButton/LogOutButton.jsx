28ae373ae5f87245b312888ada21412a01f49c80
Lucas Houska <ljhouska75@gmail.com>
2022-05-01 11:07:31 -0500

    set up link on login to bring user to the workout page

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
 function LogOutButton(props) {
+
   const dispatch = useDispatch();
+  const history = useHistory();
+
+  const moveToWorkout = () => {
+    history.push('/workout');
+  }
+
   return (
     <Button
       variant='contained'
       color='primary'
       className={props.className}
-      onClick={() => dispatch({ type: 'LOGOUT' })}
+      onClick={() => dispatch({ type: 'LOGOUT', callback: moveToWorkout })}
     >
       Log Out
     </Button>
