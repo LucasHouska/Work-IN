84dac12bc671243a20858f8d664aa0ced985103e
Lucas Houska <ljhouska75@gmail.com>
2022-05-02 14:23:23 -0500

    about to clean up again

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -4,21 +4,22 @@ import React from 'react';
 function AboutPage() {
   return (
     <div className="container">
-      <div id = 'about'>
+      <div id='about'>
         <h1>Technologies Used</h1>
         <ul className='about-list'>
-          <li>React</li>
           <li>JavaScript</li>
+          <li>React</li>
           <li>Redux</li>
           <li>Redux-Saga</li>
           <li>Express.js</li>
           <li>Node.js</li>
           <li>PostgreSQL</li>
+          <li>Passport</li>
         </ul>
         <h1>Going forward...</h1>
         <ul id='going-forward-list'>
-          <li>Show the progress of the exercises done in the workout 
-          on the finish page</li>
+          <li>Show the progress of the exercises done in the workout
+            on the finish page</li>
           <li>Add body weight, water, and mood tracker</li>
           <li>Fix minor bugs</li>
         </ul>
